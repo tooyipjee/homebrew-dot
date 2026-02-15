3583e422ce974dfef54e4bc111b2cf6ec4b480ea
tooyipjee <tooyipjee@gmail.com>
2026-02-15 18:56:49 +0000

    Update dot cask to v1.0.2

--- Casks/dot.rb
+++ Casks/dot.rb
@@ -1,6 +1,6 @@
 cask "dot" do
-  version "1.0.0"
-  sha256 "a04629e349f730724654c3c675584e2edbdb8f3c637b4918087aed5f1c22aed6"
+  version "1.0.2"
+  sha256 "7c012ff72c79ea32b156ec61ef4f54a67302f3d0ce8a6cc59d0d438f09c18d8e"
 
   url "https://github.com/tooyipjee/dot/releases/download/v#{version}/dot_#{version}_aarch64.dmg"
   name "dot"
